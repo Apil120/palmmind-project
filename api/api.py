d9ec713259dfaca8c0dc8bc89f997fc74b40abd2
Apil120 <apilisadhikari@gmail.com>
2025-11-24 21:16:47 +0545

    Refactor file upload handling and enhance PDF saving functionality

--- api/api.py
+++ api/api.py
@@ -1,7 +1,6 @@
-from fastapi import FastAPI,UploadFile,File
+from fastapi import FastAPI,UploadFile,File,status
 from fastapi.responses import RedirectResponse
-from utils.utils import read_file
-import os
+from .utils.utils import save_file
 
 app = FastAPI()
 
@@ -13,5 +12,7 @@ async def redirect():
 @app.post("/upload")
 async def upload(file:UploadFile=File(...)):
     content = await file.read()
-    extension = file.content_type.split("/")[1]
-    return read_file(content,extension=extension)
+    file_name = file.filename
+    filename, extension = file_name.split(".")[0], file_name.split(".")[1]
+    save_file(content,extension=extension,filename=filename)
+    return status.HTTP_200_OK
--- api/utils/utils.py
+++ api/utils/utils.py
@@ -1,26 +1,19 @@
-from pdfminer.high_level import extract_text
-from datetime import datetime
-import os
+# from pdfminer.high_level import extract_text
+# from datetime import datetime
+# import os
 
 
-def read_file(content: bytes, extension: str):
+def save_file(content: bytes, extension: str,filename:str):
     if extension == "pdf":
-        with open("temp.pdf", "wb") as temp_file:
+        with open(f"{filename}.pdf", "wb") as temp_file:
             temp_file.write(content)
-
-        file_content = extract_text("temp.pdf")
-        os.remove(path=os.getcwd() + "\\temp.pdf")
-        return file_content
-
-    elif extension == "txt":
-        return content.decode()
-
     else:
-        return "Invalid file format!"
+        with open("temp.txt", "w") as f:
+            f.write(content.decode())
 
 
-def get_metadata(file):
-    upload_time = datetime.now().strftime("%Y-%m-%d")
-    file_name = file.filename
+# def get_metadata(file):
+#     upload_time = datetime.now().strftime("%Y-%m-%d")
+#     file_name = file.filename
 
-    print(upload_time, file_name)
+#     print(upload_time, file_name)
