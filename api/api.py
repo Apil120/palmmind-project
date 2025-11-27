7f683d4d5eb9cd85b154196a36bc07aaa039239e
Apil120 <apiladhikari87@gmail.com>
2025-11-27 21:11:50 +0545

    Refactor file upload handling to utilize create_metadata function for improved file management

--- api/api.py
+++ api/api.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI,UploadFile,File,status
 from fastapi.responses import RedirectResponse
-from .utils.utils import save_file
+from .utils.utils import create_metadata
 
 app = FastAPI()
 
@@ -14,5 +14,5 @@ async def upload(file:UploadFile=File(...)):
     content = await file.read()
     file_name = file.filename
     filename, extension = file_name.split(".")[0], file_name.split(".")[1]
-    save_file(content,extension=extension,filename=filename)
-    return status.HTTP_200_OK
+    create_metadata(content, extension=extension, filename=filename)
+    return {"status":status.HTTP_200_OK,"message":"File upload sucessfull"}
--- api/utils/utils.py
+++ api/utils/utils.py
@@ -1,15 +1,31 @@
 # from pdfminer.high_level import extract_text
 from datetime import datetime
+
 # import os
 
 
-def save_file(content: bytes, extension: str,filename:str):
-    time_of_creation = "-"+datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    if extension == "pdf":
-        with open(f"{filename+time_of_creation}.pdf", "wb") as temp_file:
+def create_metadata(content: bytes, extension: str, filename: str):
+    details_dict = save_file(content=content, extension=extension, filename=filename)
+    time_upload = details_dict.get(
+        "creation_time", datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    )
+
+    file_name = details_dict.get("filename", "temp")
+
+    print(time_upload)
+    print(file_name)
+
+
+def save_file(content: bytes, extension: str, filename: str):
+    final_name = filename + "---" + datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    if extension.lower() == "pdf":
+        with open(f"{final_name}.pdf", "wb") as temp_file:
             temp_file.write(content)
     else:
-        with open(f"{filename+time_of_creation}.txt", "w") as f:
+        with open(f"{final_name}.txt", "w") as f:
             f.write(content.decode())
 
-#TODO:: Make a new metadata wala funciton, utilise the variables from save_file wala func.
+    return {
+        "filename": final_name.split("---")[0] + "." + extension,
+        "creation_time": final_name.split("---")[1],
+    }
