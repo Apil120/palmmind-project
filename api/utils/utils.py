456a835572bf377af1d5c97470274c4b97eb34c5
Apil120 <apiladhikari87@gmail.com>
2025-12-06 13:29:26 +0545

    Enhance chunking functionality by adding overlap handling and implementing semantic chunking with SentenceTransformer

--- api/utils/utils.py
+++ api/utils/utils.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from database.dbutils import connect_db, save_to_database,  CONFIG_DICT
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 from pdfminer.high_level import extract_text
 import os
 import re
@@ -54,9 +56,43 @@ def create_metadata(
     create_chunks(path=os.path.join("uploads", file_name),chunk_size=chunk_size,chunk_strat=chunk_strat)
     save_to_database(collection=DATABASE[METADATA_COLLECTION], object=metadata)
 
-def fixed_chunking(data,chunk_size):
+def fixed_chunking(data:str,chunk_size:int,chunk_overlap:int):
     data = re.sub(r"s+"," ",data).strip()
-    chunks = [data[i:i+chunk_size] for i in range(0, len(data),chunk_size)]
+
+    overlap = max(0,min(chunk_overlap,chunk_size-1)) #Handling cases where chunk overlap might be more than the chunk size
+    chunks = []
+    step = chunk_size - overlap
+
+    for i in range(0,len(data),step):
+        chunk = data[i:i + chunk_size]
+        chunks.append(chunk)
+        if i + chunk_size >= len(data):  # Stop once the last chunk is added
+            break
+
+
+    return chunks
+
+def semantic_chunking(data,similarity_threshold = 0.70):
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    sentences = nltk.sent_tokenize(data)
+    embeddings = model.encode(sentences)
+    chunks = []
+
+    current_chunk = [sentences[0]]
+
+    for i in range(1,len(sentences)):
+        similarity = cosine_similarity(embeddings[i].respage(1,-1),embeddings[i-1].reshape(1,-1))[0][0]
+
+        if similarity<similarity_threshold:
+            chunks.append(" ".join(current_chunk))
+            current_chunk = sentences[i]
+
+        else:
+            current_chunk.append(sentences[i])
+
+        if current_chunk:
+            chunks.append(" ".join(current_chunk))
+
     return chunks
 
 def create_chunks(chunk_size: int, chunk_strat: str, path: str): 
@@ -78,7 +114,8 @@ def create_chunks(chunk_size: int, chunk_strat: str, path: str):
        return chunks
     
 
-    semantic_chunking(data,chunk_size) #TODO: Implement Semantic chunking
+    chunks = semantic_chunking(data)
+    return chunks
     
     
 
